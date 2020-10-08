040ae2b3d7439d8d43d285317661bc91dae94fcf
Amber <a.rignell16@ejm.org>
2020-10-08 18:00:30 +0100

    switch links from localhost backend to our backend hosted on heroku

--- src/utils/addData.js
+++ src/utils/addData.js
@@ -3,7 +3,7 @@ function addData(id, glucose_reading) {
   let time = d.getTime();
   let data = { id, glucose_reading, time };
   console.log(data);
-  fetch('http://localhost:3000/api/store-data', {
+  fetch('https://jalf.herokuapp.com/api/store-data', {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
